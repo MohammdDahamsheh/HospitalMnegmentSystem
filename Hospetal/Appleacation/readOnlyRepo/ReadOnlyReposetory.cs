a38f524dc494a55ab392dc37c149e5106e4816ac
mohammd jehad <mhmddahamsheh@gmail.com>
2026-04-03 21:42:42 +0300

    Add generic repository and unit of work patterns
    Introduced IReadOnlyReposotory, IReposotory, and IUnitOfWork interfaces with their implementations to enable generic data access and transaction management. Registered these services in DI, updated project references, and removed a circular dependency for improved maintainability and testability.

---
+++ Hospetal/Appleacation/readOnlyRepo/ReadOnlyReposetory.cs
@@ -0,0 +1,42 @@
+﻿using Infrustracher;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appleacation.readOnlyRepo
+{
+    public class ReadOnlyReposetory<T> : IReadOnlyReposotory<T> where T : class
+    {
+        private HospitalContext context;
+        private DbSet<T> dbSet;
+
+        public ReadOnlyReposetory(HospitalContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+           var result = await dbSet.ToListAsync();
+            if (result == null)
+            {
+                throw new Exception("No data found");
+            }
+            return result;
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var result=await dbSet.FindAsync(id);
+            if (result == null)
+            {
+                throw new Exception("No data found");
+            }
+            return result;
+        }
+    }
+}
